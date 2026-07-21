0e29d15f3834955c2d6e2093fc6f2b94412c883f
y82003115-ux <y82003115@gmail.com>
2026-07-21 16:11:35 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ys-love-v25-20260721';
+const CACHE='ys-love-v26-20260721';
 const ASSETS=['./','./index.html','./invites.js','./pwa.js','./home-ui.js','./mobile-ui.css','./manifest.webmanifest','./app-icon-3d.png','./romantic-3d-bg.png','./cinema-couple-final-v4.png','./cinema-effects-3d.png','./cinema-mic-assets-3d.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
